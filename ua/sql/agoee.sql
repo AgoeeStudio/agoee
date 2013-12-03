cf9c5982679fb1303223bab2ab49684d3019558f
syupo <syupo.lee@gmail.com>
2013-12-03 10:03:28 +0800

    fix unique index

--- ua/sql/agoee.sql
+++ ua/sql/agoee.sql
@@ -10,21 +10,8 @@
 # Generation Time: 2013-11-24 20:48:09 +0000
 # ************************************************************
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Dump of table tbl_account
-# ------------------------------------------------------------
-
+-- table tbl_account 账户表
 DROP TABLE IF EXISTS `tbl_account`;
-
 CREATE TABLE `tbl_account` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(36) CHARACTER SET ascii NOT NULL DEFAULT '' COMMENT '通用唯一识别码',
@@ -34,18 +21,10 @@ CREATE TABLE `tbl_account` (
   `verify_code` varchar(64) CHARACTER SET ascii DEFAULT '' COMMENT 'Base64激活验证码，含验证超时信息，NULL为已激活',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UINDEX_UID` (`uuid`,`username`,`email`)
+  UNIQUE KEY `UINDEX_UID` (`uuid`),
+  UNIQUE KEY `UINDEX_NAME` (`username`),
+  UNIQUE KEY `UINDEX_EMAIL` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 -- OAuth相关
 create table oauth_client_details (
